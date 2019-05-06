cf102be91227d2d48690ff125d7825655a09f628
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-06 20:33:19 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function submitIt(){
   $('form').on("submit", function(){
     alert("Your form is going to be submitted now.")
   });
-}
+};
 $(document).ready(function(){
 
 // call functions here
