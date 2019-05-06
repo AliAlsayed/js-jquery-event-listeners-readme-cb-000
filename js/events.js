b20d03124e8f3a1e564125f6c5d437c6a0bd6561
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-06 20:26:46 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,12 @@ function getIt(){
   $('p').on("click", function(){
     alert("Hey!");
   });
+};
+
+function frameIt(){
+  $(document).on("load", function(){
+    
+  })
 }
 $(document).ready(function(){
 
